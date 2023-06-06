2a51f989a5de3a7782a30f11e368005df9bc525b
Fazel034 <135029880+Fazel034@users.noreply.github.com>
2023-06-06 10:57:21 +0330

    modify structure project

--- bottom_navigation/app/src/main/java/com/example/bottom_navigation/MainActivity.kt
+++ bottom_navigation/app/src/main/java/com/example/bottom_navigation/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.bottom_navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.bottom_navigation.ui.theme.Bottom_navigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,14 +36,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(){
+    val navController = rememberNavController()
+    Scaffold(bottomBar = { MyBottomNavigation(navController = navController)}) {
+        Box(modifier = Modifier.padding(it)){
+            NavHost(navController = navController, startDestination = Login.route ){
+                composable(Login.route){
+                    Login(navController)
+                }
+                composable(Welcome.route){
+                    Welcome(navController)
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Bottom_navigationTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
